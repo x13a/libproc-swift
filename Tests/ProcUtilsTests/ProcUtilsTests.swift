d64ec84f824377dbfdde521e42e098b66e7efe22
lucky <>
2021-07-08 11:25:34 +0300

    refactor

--- Tests/ProcUtilsTests/ProcUtilsTests.swift
+++ Tests/ProcUtilsTests/ProcUtilsTests.swift
@@ -1,7 +1,7 @@
     import XCTest
     @testable import ProcUtils
 
-    final class ProcTests: XCTestCase {
+    final class ProcUtilsTests: XCTestCase {
         func testExample() {
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
